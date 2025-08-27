1e1091031ba9b8423454820cd5a8aad6295425fd
colmmandev <colmenaresmanueljt@gmail.com>
2025-08-27 15:43:56 -0300

    Mini Servidor

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello Wordl");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const responseData = { message: "Hola desde mi primer endpoint!" };
+    res.end(JSON.stringify(responseData)); // Asegúrate de enviar una cadena JSON
+  }
+  if (req.url === "/about" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const responseData = { message: "Estoy aprendiendo backend" };
+    res.end(JSON.stringify(responseData)); // Asegúrate de enviar una cadena JSON
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Ruta no encontrada");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
